089ef2144b7f43e9f0c2011c152c5c9fca2c2bfe
Ankita Ghosh <141432077+ankitasure23@users.noreply.github.com>
2026-01-20 15:03:28 +0530

    Update App.jsx

--- Pragya/src/App.jsx
+++ Pragya/src/App.jsx
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
 import { useState } from 'react'
-
-=======
->>>>>>> 41dfc530ac7433df6162c638a904184f5ed17216
 import './App.css'
 import Navigation from "./Pages/Navigation.jsx"
 import { Zap, Star, Flame } from 'lucide-react';
 
 function App() {
-<<<<<<< HEAD
-  
-
-=======
   return (
     <div className="min-h-screen bg-linear-to-br from-purple-50 via-blue-50 to-cyan-50">
       <Navigation />
     </div>
   );
->>>>>>> 41dfc530ac7433df6162c638a904184f5ed17216
 }
 
 export default App
